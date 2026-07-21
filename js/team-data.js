a1ca8e9360b43e6d662dcb80ef58a8238273c227
gualle <yutonw37@uci.edu>
2026-07-20 21:30:50 -0700

    Remove WEAI Lab prefix from team roles

--- js/team-data.js
+++ js/team-data.js
@@ -9,84 +9,84 @@
 const teamMembers = [
     {
         name: "Pooya Sareh",
-        role: "WEAI Lab Senior Academic Advisor",
+        role: "Senior Academic Advisor",
         image: "img/team/pooya-sareh.png",
         additionalInfo: "Associate Professor (Senior Lecturer), Newcastle University; Director, Creative Design Engineering Lab (CDEL)",
         links: {}
     },
     {
         name: "Cecilia Elisabet García Cena",
-        role: "WEAI Lab Senior Academic Advisor",
+        role: "Senior Academic Advisor",
         image: "img/team/cecilia-garcia-cena.jpg",
         additionalInfo: "Associate Professor, Department of Electrical, Electronic, Automatic Control, and Applied Physics Engineering, ETSIDI, Technical University of Madrid",
         links: {}
     },
     {
         name: "Ming Cao",
-        role: "WEAI Lab Senior Academic Advisor & Co-founder",
+        role: "Senior Academic Advisor & Co-founder",
         image: "img/team/ming-cao.png",
         additionalInfo: "Dean, School of Digital Technology and Creative Design, Jiangnan University",
         links: {}
     },
     {
         name: "Liyuan Bao",
-        role: "WEAI Lab Founder & Research Director",
+        role: "Founder & Research Director",
         image: "img/team/liyuan-bao.jpg",
         additionalInfo: "Director, Department of Interaction Design, Jiangnan University",
         links: {}
     },
     {
         name: "Xin Chen",
-        role: "WEAI Lab Founder & Principal Researcher",
+        role: "Founder & Principal Researcher",
         image: "img/team/xin-chen.png",
         additionalInfo: "Ph.D. Researcher, Technical University of Madrid",
         links: {}
     },
     {
         name: "Xiaofeng Han",
-        role: "WEAI Lab Academic Advisor",
+        role: "Academic Advisor",
         image: "img/team/xiaofeng-han.jpg",
         additionalInfo: "Ph.D. Researcher, Technical University of Madrid",
         links: {}
     },
     {
         name: "Lianye Zhang",
-        role: "WEAI Lab Research Assistant",
+        role: "Research Assistant",
         image: "img/team/lianye-zhang.jpg",
         additionalInfo: "M.Sc. in Information Management, The Hong Kong Polytechnic University",
         links: {}
     },
     {
         name: "Lue Shen",
-        role: "WEAI Lab Research Assistant",
+        role: "Research Assistant",
         image: "img/team/lue-shen.png",
         additionalInfo: "M.Des. in Interaction Design, Jiangnan University",
         links: {}
     },
     {
         name: "Yujie Gu",
-        role: "WEAI Lab Research Assistant",
+        role: "Research Assistant",
         image: "img/team/yujie-gu.jpg",
         additionalInfo: "M.A. in Performance and Interaction Design, University College London",
         links: {}
     },
     {
         name: "Yutong Wu",
-        role: "WEAI Lab Research Assistant Intern",
+        role: "Research Assistant Intern",
         image: "img/team/yutong-wu.png",
         additionalInfo: "B.S. student in Computer Science, University of California, Irvine",
         links: {}
     },
     {
         name: "Xinrui Li",
-        role: "WEAI Lab Research Assistant Intern",
+        role: "Research Assistant Intern",
         image: "img/team/xinrui-li.jpg",
         additionalInfo: "B.A. student in Art, University of California, Irvine",
         links: {}
     },
     {
         name: "Yan Wang",
-        role: "WEAI Lab Research Assistant Intern",
+        role: "Research Assistant Intern",
         image: "img/team/yan-wang.jpg",
         additionalInfo: "B.F.A. student in Designed Objects, School of the Art Institute of Chicago",
         links: {}
